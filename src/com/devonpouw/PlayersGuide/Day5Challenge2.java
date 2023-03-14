e77211626bd755672f97cd55fca1a4caa31e10c1
Devon Pouw <devon_pouw@hotmail.com>
2023-03-14 11:09:37 +0100

    14-3-2023 version

--- src/com/devonpouw/PlayersGuide/Day5Challenge2.java
+++ src/com/devonpouw/PlayersGuide/Day5Challenge2.java
@@ -1,4 +1,25 @@
 package com.devonpouw.PlayersGuide;
 
+import java.util.Scanner;
+
 public class Day5Challenge2 {
+
+    /*Scanner input = new Scanner(System.in);
+    //Area = base x height / 2
+    double height = input.nextDouble();
+    double base = input.nextDouble();
+    double area = (height*base/2);*/
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Area = base x height / 2
+        System.out.println("What is the height of the triangle?");
+        double height = input.nextDouble();
+        System.out.println("What is the base of the triangle?");
+        double base = input.nextDouble();
+        double area = (height*base/2);
+
+        System.out.println("The area of the triangle is: " + area);
+    }
 }
