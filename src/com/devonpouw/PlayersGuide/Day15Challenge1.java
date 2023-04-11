7e46ef4296981d0e2ce7ab4616de8efb3c615509
Devon Pouw <devon_pouw@hotmail.com>
2023-04-11 16:44:28 +0200

    11april

--- src/com/devonpouw/PlayersGuide/Day15Challenge1.java
+++ src/com/devonpouw/PlayersGuide/Day15Challenge1.java
@@ -1,4 +1,18 @@
 package com.devonpouw.PlayersGuide;
 
 public class Day15Challenge1 {
+    public static void main(String[] args) {
+        int[] array = {4, 51, -7, 13, -99, 15, -8, 45, 90};
+        int currentSmallest = Integer.MAX_VALUE; // Start higher than anything in the array.
+        for (int i : array) {
+            if (i < currentSmallest)
+                currentSmallest = i;
+        }
+        System.out.println(currentSmallest);
+
+        int total = 0;
+        for (int i : array) total += i;
+        double average = (double) total / array.length;
+        System.out.println(average);
+    }
 }
