ce0b708795772dcf11c7a64c1849e456882e6c38
Devon Pouw <devon_pouw@hotmail.com>
2023-05-01 16:30:33 +0200

    1mei

--- src/com/devonpouw/SemsAssignments/JavaFundamentals/IntermediateLevel/Task1.java
+++ src/com/devonpouw/SemsAssignments/JavaFundamentals/IntermediateLevel/Task1.java
@@ -1,4 +1,14 @@
 package com.devonpouw.SemsAssignments.JavaFundamentals.IntermediateLevel;
 
+import java.util.Arrays;
+
 public class Task1 {
+    public static int min(int[] numbers) {
+        return Arrays.stream(numbers).min().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        int test = min(new int[]{1, 5, 102, 95});
+        System.out.println(test);
+    }
 }
---
+++ src/com/devonpouw/SemsAssignments/JavaFundamentals/IntermediateLevel/Task2.java
@@ -0,0 +1,18 @@
+package com.devonpouw.SemsAssignments.JavaFundamentals.IntermediateLevel;
+
+public class Task2 {
+    public static int countEven(int[] numbers) {
+        int evenNums = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenNums++;
+            }
+        }
+        return evenNums;
+    }
+
+    public static void main(String[] args) {
+        int test = countEven(new int[]{1, 2, 5, 10, 32, 103});
+        System.out.println(test);
+    }
+}
